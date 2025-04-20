b0f0638867400b07b1ca6ad918e0097eba6464f2
Med. A. Bada <151136456+Thxr21@users.noreply.github.com>
2025-04-21 00:31:32 +0100

    Update Company.java

--- Springboot/src/main/java/com/projinteg/GesEvents/entities/Company.java
+++ Springboot/src/main/java/com/projinteg/GesEvents/entities/Company.java
@@ -10,16 +10,60 @@ public class Company {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+
+    private String responsable;
+    private String category;
+
+    private String email;
+    private String numTel;
+
     private String password;
+
     private boolean verified = false;
 
     public Company() {
 
     }
 
-    public Company(String password, String name) {
+    public Company(String password, String numTel, String email, String category, String responsable, String name) {
         this.password = password;
+        this.numTel = numTel;
+        this.email = email;
+        this.category = category;
+        this.responsable = responsable;
         this.name = name;
+    }
+
+    public String getResponsable() {
+        return responsable;
+    }
+
+    public void setResponsable(String responsable) {
+        this.responsable = responsable;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNumTel() {
+        return numTel;
+    }
+
+    public void setNumTel(String numTel) {
+        this.numTel = numTel;
     }
 
     public Long getId() {
