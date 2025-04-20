5f74c744b346c31940501d1cd53125d842da450a
WassimHed <hedhliwassim003@gmail.com>
2025-04-20 12:30:45 +0100

    Fixed spring boot project(version,dependencies...)

--- Springboot/src/test/java/com/projinteg/GesEvents/GesEventsApplicationTests.java
+++ Springboot/src/test/java/com/projinteg/GesEvents/GesEventsApplicationTests.java
@@ -1,13 +1,13 @@
-package com.proj.springboot;
+package com.projinteg.GesEvents;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SpringbootApplicationTests {
+class GesEventsApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	void contextLoads() {
+	}
 
 }
