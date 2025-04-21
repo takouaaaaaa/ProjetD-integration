dab0cabc7fc3e9086041f8c1eaa88412d57230b1
mecherguirabeb <153821221+mecherguirabeb@users.noreply.github.com>
2025-04-21 22:18:45 +0200

    Ajout consultation liste companies avec masquage password

--- Springboot/src/main/java/com/projinteg/GesEvents/controller/CompanyController.java
+++ Springboot/src/main/java/com/projinteg/GesEvents/controller/CompanyController.java
@@ -4,10 +4,13 @@ import com.projinteg.GesEvents.entities.Company;
 import com.projinteg.GesEvents.service.CompanyService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/companies")
@@ -26,5 +29,18 @@ public class CompanyController {
         }
     }
 
+    
+
+    @GetMapping
+    public List<Company> getAllCompanies() {
+        return companyService.getAllCompanies();
+    }
+    
+    @GetMapping("/{id}")
+    public ResponseEntity<Company> getCompanyById(@PathVariable Long id) {
+        Company company = companyService.getCompanyById(id)
+                          .orElseThrow(() -> new RuntimeException("Non trouvé"));
+        return ResponseEntity.ok(company);
+    }
 
 }
--- Springboot/src/main/java/com/projinteg/GesEvents/service/CompanyService.java
+++ Springboot/src/main/java/com/projinteg/GesEvents/service/CompanyService.java
@@ -5,6 +5,8 @@ import com.projinteg.GesEvents.entities.Company;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CompanyService {
@@ -18,6 +20,20 @@ public class CompanyService {
     public Company register(Company company) {
         company.setPassword(passwordEncoder.encode(company.getPassword()));
         return companyRepository.save(company);
+    }
+
+    public List<Company> getAllCompanies() {
+        return companyRepository.findAll().stream()
+                .peek(company -> company.setPassword(null))
+                .toList();
+    }
+    
+    public Optional<Company> getCompanyById(Long id) {
+        return companyRepository.findById(id)
+                .map(company -> {
+                    company.setPassword(null);
+                    return company;
+                });
     }
 
 }
