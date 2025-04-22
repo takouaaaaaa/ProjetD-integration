efab0b513711fb28cd331f843f8f72bb52406647
Med. A. Bada <151136456+Thxr21@users.noreply.github.com>
2025-04-22 15:51:19 +0100

    (Laravel) added try/catch to register

--- Laravel/app/Http/Controllers/CompanyController.php
+++ Laravel/app/Http/Controllers/CompanyController.php
@@ -6,20 +6,25 @@ use App\Models\Company;
 use App\Enums\Role;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
-
 class CompanyController extends Controller
 {
     public function register(Request $request)
     {
+
         // Validate incoming data, including password confirmation
-        $data = $request->validate([
-            'name'             => 'required|string|max:255',
-            'responsable'      => 'required|string|max:255',
-            'numTel'           => 'required|string|max:50',
-            'email'            => 'required|email|unique:companies,email',
-            'category'         => 'required|string|max:100',
-            'password'         => 'required|string|min:6|confirmed',
-        ]);
+        try {
+            $data = $request->validate([
+                'name'             => 'required|string|max:255',
+                'responsable'      => 'required|string|max:255',
+                'numTel'           => 'required|string|unique:companies,numTel|max:50',
+                'email'            => 'required|email|unique:companies,email',
+                'category'         => 'required|string|max:100',
+                'password'         => 'required|string|min:6',
+            ]);
+        } catch (\Throwable $th) {
+            return response()->json(['message' => $th->getMessage()], 404);
+            
+        }
 
         // Hash the password
         $data['password'] = Hash::make($data['password']);
--- Laravel/routes/api.php
+++ Laravel/routes/api.php
@@ -4,5 +4,5 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CompanyController;
 
 Route::post('/companies/register', [CompanyController::class, 'register']);
-Route::get('/companies', [CompanyController::class, 'getAllCompanies']);
-Route::get('/companies/{id}', [CompanyController::class, 'getCompanyById']);
+Route::get('/companies/getAll', [CompanyController::class, 'getAllCompanies']);
+Route::get('/companies/getById/{id}', [CompanyController::class, 'getCompanyById']);
